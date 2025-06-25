bb956d4836e9b5c498ff6ba89d9fbc62d2653fce
kakahimita <kakahimita@gmail.com>
2025-06-25 08:04:40 -0300

    estudos: controller PUT

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import "doteenv/config";
+import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = 3000;
--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -1,11 +1,39 @@
-import livro from "./models/Livros.js";
+import livro from "../models/Livro.js";
 
 class LivroController {
-
-    static async listarLivros (req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+  static async listarLivros(req, res) {
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha na requisição` });
     }
-};
+  }
+
+  static async listarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livro);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha na requisição do livro` });
+    }
+  }
+
+  static async cadastrarLivro(req, res) {
+    try {
+      const novoLivro = await livro.create(req.body);
+      res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha ao cadastrar livro` });
+    }
+  }
+}
 
 export default LivroController;
