eceb0ab826f3686866b110ff3efe4add7bf15be5
kakahimita <kakahimita@gmail.com>
2026-01-09 20:39:10 -0300

    feat: tratando erros de pagina 404

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,3 +1,4 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 import autores from "../models/Autor.js";
 // import moongose from "mongoose";
 
@@ -21,7 +22,7 @@ class AutorController {
       if (autorResultado !== null) {
         res.status(200).send(autorResultado);
       } else {
-        res.status(404).send({ message: "Id do Autor não localizado." });
+        next(new NaoEncontrado("Id do Autor não localizado."));
       }
     } catch (erro) {
       next(erro);
@@ -46,9 +47,13 @@ class AutorController {
     try {
       const id = req.params.id;
 
-      await autores.findByIdAndUpdate(id, { $set: req.body });
+      const autorAtualizado = await autores.findByIdAndUpdate(id, { $set: req.body });
 
-      res.status(200).send({ message: "Autor atualizado com sucesso" });
+      if (autorAtualizado !== null) {
+        res.status(200).send({ message: "Autor atualizado com sucesso" });
+      } else {
+        next(new NaoEncontrado("Id do Autor não localizado."));
+      }
     } catch (erro) {
       next(erro);
     }
@@ -58,9 +63,13 @@ class AutorController {
     try {
       const id = req.params.id;
 
-      await autores.findByIdAndDelete(id);
+      const autorExcluido = await autores.findByIdAndDelete(id);
 
-      res.status(200).send({ message: "Autor removido com sucesso" });
+      if (autorExcluido !== null) {
+        res.status(200).send({ message: "Autor removido com sucesso" });
+      } else {
+        next(new NaoEncontrado("Id do Autor não localizado."));
+      }
     } catch (erro) {
       next(erro);
     }
